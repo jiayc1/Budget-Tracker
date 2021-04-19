d9064ca64175b97492f1f95416340fee02e2d4a9
Jess <jess.chen0502@gmail.com>
2021-04-18 20:35:27 -0700

    deploying

--- server.js
+++ server.js
@@ -15,10 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://jiayc1:Max0310!@cluster0.cizg6.mongodb.net/budgetracker?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb+srv://jiayc1:Max0310!@cluster0.cizg6.mongodb.net/budgetracker?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
